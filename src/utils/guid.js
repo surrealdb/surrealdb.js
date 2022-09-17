5418785c5e31d7b28b03a07c5fb28a7aed3bf061
Jareer Abdullah <jforeverything2007@gmail.com>
2022-09-17 09:19:44 +0200

    Eliminate the need for `nanoid`

--- src/utils/guid.js
+++ src/utils/guid.js
@@ -1,6 +1,18 @@
-import { customAlphabet } from 'nanoid';
-const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 10);
+const nanoid = (chars, length) => {
+	let id = [];
+	let charlist = chars.split("");
 
-export default function() {
-	return nanoid();
+	while (id.length < length) {
+		const random = charlist[Math.floor(Math.random() * charlist.length)];
+
+		if (id[id.length - 1] != random) {
+			id.push(random);
+		}
+	}
+
+	return id.join("");
+};
+
+export default function () {
+	return nanoid("1234567890abcdefghijklmnopqrstuvwxyz", 10);
 }
