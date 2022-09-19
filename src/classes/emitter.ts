2c858676e605453ecfb4e183bea15059cdca1233
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2022-09-20 01:24:49 +0800

    Improve emitter class

--- src/classes/emitter.ts
+++ src/classes/emitter.ts
@@ -1,49 +1,67 @@
 // deno-lint-ignore-file no-explicit-any
+export type EventName = string | symbol;
 export type Listener = (this: Emitter, ...args: any[]) => void;
 
+export default interface Emitter {
+	addListener(eventName: EventName, listener: Listener): this;
+	off(eventName: EventName, listener: Listener): this;
+}
+
 export default class Emitter {
-	#events: Record<string, Listener[]> = {};
+	#events: Map<EventName, Listener[]> = new Map();
 
-	on(e: string, func: Listener) {
-		if (typeof this.#events[e] !== "object") {
-			this.#events[e] = [];
-		}
-		this.#events[e].push(func);
+	static {
+		this.prototype.addListener = this.prototype.on;
+		this.prototype.off = this.prototype.removeListener;
 	}
 
-	off(e: string, func: Listener) {
-		if (typeof this.#events[e] === "object") {
-			const idx = this.#events[e].indexOf(func);
-			if (idx > -1) {
-				this.#events[e].splice(idx, 1);
-			}
-		}
-	}
-
-	once(e: string, func: Listener) {
-		this.on(e, function f(...args) {
-			this.off(e, f);
-			func.apply(this, args);
-		});
-	}
-
-	emit(e: string, ...args: any[]) {
-		if (typeof this.#events[e] === "object") {
-			this.#events[e].forEach((func) => {
-				func.apply(this, args);
-			});
-		}
-	}
-
-	removeAllListeners(e?: string) {
-		if (e) {
-			if (typeof this.#events[e] === "object") {
-				this.#events[e] = [];
-			}
+	on(eventName: EventName, listener: Listener): this {
+		const listeners = this.#events.get(eventName);
+		if (listeners !== undefined) {
+			listeners.push(listener);
 		} else {
-			for (const e in this.#events) {
-				this.#events[e] = [];
+			this.#events.set(eventName, [listener]);
+		}
+		return this;
+	}
+
+	removeListener(eventName: EventName, listener: Listener): this {
+		const events = this.#events.get(eventName);
+		if (events !== undefined) {
+			const idx = events.indexOf(listener);
+			if (idx > -1) {
+				if (events.length === 1) {
+					this.#events.delete(eventName);
+				} else {
+					events.splice(idx, 1);
+				}
 			}
 		}
+		return this;
+	}
+
+	once(eventName: EventName, listener: Listener): this {
+		this.on(eventName, function once(...args) {
+			this.removeListener(eventName, listener);
+			listener.apply(this, args);
+		});
+		return this;
+	}
+
+	emit(eventName: EventName, ...args: any[]): this {
+		const listeners = this.#events.get(eventName);
+		if (listeners !== undefined) {
+			listeners.forEach((listener) => listener.apply(this, args));
+		}
+		return this;
+	}
+
+	removeAllListeners(eventName?: EventName): this {
+		if (eventName !== undefined) {
+			this.#events.delete(eventName);
+		} else {
+			this.#events.clear();
+		}
+		return this;
 	}
 }
