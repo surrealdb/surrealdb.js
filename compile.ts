10e51f1a50b348df973ffa5d540e6edd96a399e8
mathe42 <2pi_r2@gmx.de>
2022-09-21 17:54:37 +0200

    update how isomorphic-ws is handled

--- compile.ts
+++ compile.ts
@@ -3,44 +3,53 @@ import { version } from "./project.json" assert { type: "json" };
 
 await emptyDir("./npm");
 
-Deno.copyFileSync('./src/classes/socket.ts', './socket.ts')
-Deno.writeTextFileSync('./src/classes/socket.ts', 'import WebSocket from "isomorphic-ws"\n' + Deno.readTextFileSync('./src/classes/socket.ts'))
+// Add isomorphic-ws
+const socketCode = Deno.readTextFileSync('./src/classes/socket.ts')
+Deno.writeTextFileSync('./src/classes/socket.ts', 'import WebSocket from "isomorphic-ws"\n' + socketCode)
 
-await build({
-  entryPoints: ["./src/index.ts"],
-  outDir: "./npm",
-  shims: {
-    // see JS docs for overview and more options
-    deno: false,
-    webSocket: false
-  },
-  package: {
-    // package.json properties
-    name: "surrealdb.js",
-    version: version,
-    description: "Javascript driver for SurrealDB",
-    license: "Apache 2.0",
-    repository: {
-      type: "git",
-      url: "https://github.com/surrealdb/surrealdb.js.git"
-    },
-    author: {
-      name: "Tobie Morgan Hitchcock",
-      url: "https://surrealdb.com"
-    },
-    dependencies: {
-      "isomorphic-ws": "^5.0.0",
-      "ws": "^8.8.1"
-    },
-    devDependencies: {
-      "@types/ws": "8.5.3"
-    }
-  },
-});
+let err;
 
-// post build steps
-Deno.copyFileSync("LICENSE", "npm/LICENSE");
-Deno.copyFileSync("README.md", "npm/README.md");
+try {
+  await build({
+    entryPoints: ["./src/index.ts"],
+    outDir: "./npm",
+    shims: {
+      // see JS docs for overview and more options
+      deno: false,
+      webSocket: false
+    },
+    package: {
+      // package.json properties
+      name: "surrealdb.js",
+      version: version,
+      description: "Javascript driver for SurrealDB",
+      license: "Apache 2.0",
+      repository: {
+        type: "git",
+        url: "https://github.com/surrealdb/surrealdb.js.git"
+      },
+      author: {
+        name: "Tobie Morgan Hitchcock",
+        url: "https://surrealdb.com"
+      },
+      dependencies: {
+        "isomorphic-ws": "^5.0.0",
+        "ws": "^8.8.1"
+      },
+      devDependencies: {
+        "@types/ws": "8.5.3"
+      }
+    },
+  });  
 
-Deno.copyFileSync('./socket.ts', './src/classes/socket.ts')
-Deno.removeSync('./socket.ts')
+  // post build steps
+  Deno.copyFileSync("LICENSE", "npm/LICENSE");
+  Deno.copyFileSync("README.md", "npm/README.md");
+} catch (ex) {
+  err = ex
+}
+
+// remove isomorphic-ws
+Deno.writeTextFileSync('./src/classes/socket.ts', socketCode)
+
+if(err) throw err;
