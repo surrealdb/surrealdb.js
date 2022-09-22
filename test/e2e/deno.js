67d1b3f0e8294765ecd4ded47120e22d9592cc22
oberbeck <20164654+oberbeck@users.noreply.github.com>
2022-09-22 19:49:35 +0200

    add basic e2e test

--- test/e2e/deno.js
+++ test/e2e/deno.js
@@ -1,8 +1,8 @@
 import Surreal from "../../src/index.ts";
 import handler from "./shared.js";
 
-const client = new Surreal('http://127.0.0.1/rtc')
+const client = new Surreal("http://127.0.0.1:8000/rpc");
 
-await handler(client)
+await handler(client);
 
-client.close()
+client.close();
