a6a1344e502c18f5e4281379416a308ebf304083
oberbeck <20164654+oberbeck@users.noreply.github.com>
2022-09-25 19:49:56 +0200

    add doc to runners from https://surrealdb.com/docs/surrealql/statements/info

--- src/metainfo.ts
+++ src/metainfo.ts
@@ -2,6 +2,11 @@ import Surreal from "./index.ts";
 
 type SURREAL_QUERY = string;
 
+/**
+ * # System information
+ * The top-level KV command returns information regarding the namespaces which exists within the SurrealDB system.
+ * @note You must be authenticated as a top-level root user to execute this command.
+ */
 export function system() {
 	type Payload = {
 		ns: {
@@ -11,6 +16,12 @@ export function system() {
 	return (surreal: Surreal) => surreal.query<Payload>("INFO FOR KV;");
 }
 
+/**
+ * # Namespace information
+ * The NS or NAMESPACE command returns information regarding the logins, tokens, and databases under a specific Namespace.
+ * @note You must be authenticated as a top-level root user, or a namespace user to execute this command.
+ * @note You must have a NAMESPACE selected before running this command.
+ */
 export function namespace() {
 	type Payload = {
 		db: {
@@ -23,6 +34,12 @@ export function namespace() {
 	return (surreal: Surreal) => surreal.query<Payload>("INFO FOR NS;");
 }
 
+/**
+ * # Database information
+ * The DB or DATABASE command returns information regarding the logins, tokens, and scopes, and tables under a specific Database.
+ * @note You must be authenticated as a top-level root user, a namespace user, or a database user to execute this command.
+ * @note You must have a NAMESPACE and a DATABASE selected before running this command.
+ */
 export function database() {
 	type Payload = {
 		dl: unknown;
@@ -35,6 +52,12 @@ export function database() {
 	return (surreal: Surreal) => surreal.query<Payload>("INFO FOR DB;");
 }
 
+/**
+ * # Scope information
+ * The SCOPE command returns information regarding the tokens configured under a specific Scope.
+ * @note You must be authenticated as a top-level root user, a namespace user, or a database user to execute this command.
+ * @note You must have a NAMESPACE and a DATABASE selected before running this command.
+ */
 export function scope(scope: string) {
 	type Payload = {
 		st: unknown;
@@ -43,6 +66,12 @@ export function scope(scope: string) {
 		surreal.query<Payload>("INFO FOR SCOPE $scope;", { scope });
 }
 
+/**
+ * # Table information
+ * The TABLE command returns information regarding the events, fields, indexes, and foreign table configurations on a specific Table.
+ * @note You must be authenticated as a top-level root user, a namespace user, or a database user to execute this command.
+ * @note You must have a NAMESPACE and a DATABASE selected before running this command.
+ */
 export function table(table: string) {
 	type Payload = {
 		ev: unknown;
