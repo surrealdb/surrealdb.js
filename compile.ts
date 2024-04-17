77ae9a4c2113914c7370de20c94036d1aff41a73
Micha de Vries <micha@devrie.sh>
2024-04-18 00:48:04 +0200

    assert > with

--- compile.ts
+++ compile.ts
@@ -1,5 +1,5 @@
 import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
-import project from "./project.json" assert { type: "json" };
+import project from "./project.json" with { type: "json" };
 
 await emptyDir("./npm");
 
