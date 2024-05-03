432cd69068e5960274df650ce8eca59bc567dd28
Julian Mills <julian.exodius@gmail.com>
2024-05-03 11:21:05 +0200

    fix capitalization

--- tests/integration/tests/connection.ts
+++ tests/integration/tests/connection.ts
@@ -1,7 +1,7 @@
 import { createSurreal } from "../surreal.ts";
 import { assertEquals } from "https://deno.land/std@0.223.0/assert/mod.ts";
 
-Deno.test("Check version", async () => {
+Deno.test("check version", async () => {
 	const surreal = await createSurreal();
 	const res = await surreal.version();
 
