082efd00b0b690e0bd36248f1e14e1776bf06a1b
Sacha Weatherstone <sachaw100@hotmail.com>
2024-05-31 01:10:28 +1000

    Fix SemVer tests

--- tests/unit/isVersionSupported.ts
+++ tests/unit/isVersionSupported.ts
@@ -1,29 +1,30 @@
 import { assertEquals } from "@std/assert";
+import { parse } from "@std/semver";
 import { isVersionSupported } from "../../src/library/versionCheck.ts";
 
 Deno.test("isVersionSupported", () => {
 	assertEquals(
-		isVersionSupported("1.0.0"),
+		isVersionSupported(parse("1.0.0")),
 		false,
 		"1.0.0 should be unsupported",
 	);
 	assertEquals(
-		isVersionSupported("1.4.1"),
+		isVersionSupported(parse("1.4.1")),
 		false,
 		"1.4.1 should be unsupported",
 	);
 	assertEquals(
-		isVersionSupported("1.4.2"),
+		isVersionSupported(parse("1.4.2")),
 		true,
 		"1.4.2 should be supported",
 	);
 	assertEquals(
-		isVersionSupported("1.99.99"),
+		isVersionSupported(parse("1.99.99")),
 		true,
 		"1.99.99 should be supported",
 	);
 	assertEquals(
-		isVersionSupported("2.0.0"),
+		isVersionSupported(parse("2.0.0")),
 		false,
 		"2.0.0 should be unsupported",
 	);
