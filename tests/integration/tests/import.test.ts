960517b2b1b562e8fafc3ed4c45acfdcf55e619f
Julian Mills <julian.exodius@gmail.com>
2025-02-07 13:00:23 +0100

    add minimum version to test

--- tests/integration/tests/import.test.ts
+++ tests/integration/tests/import.test.ts
@@ -1,12 +1,16 @@
 import { describe, expect, test } from "bun:test";
 import { setupServer } from "../surreal.ts";
+import { fetchVersion } from "../helpers.ts";
+import { compareVersions } from "compare-versions";
 
 const { createSurreal } = await setupServer();
 
 describe("import", async () => {
 	const surreal = await createSurreal();
+	const version = await fetchVersion(surreal);
+	const runTest = compareVersions(version, "2.0.0") >= 0;
 
-	test("basic", async () => {
+	test.if(runTest)("basic", async () => {
 		await surreal.import(/* surql */ `
 			CREATE foo:1 CONTENT { hello: "world" };
 		`);
