3d552aeb25c1e5c8481045ac07c4599c96d0dd0d
Julian Mills <julian.exodius@gmail.com>
2025-06-02 14:18:17 +0200

    jsr package namespace

--- scripts/build/jsr.ts
+++ scripts/build/jsr.ts
@@ -1,13 +1,16 @@
 import { join } from "node:path";
 import { readPackageJson } from "scripts/utils/package";
 
+const NS = "@surrealdb";
+
 export async function generateJSR(pkg: string): Promise<void> {
 	const { name, version } = await readPackageJson(pkg);
 
 	const jsrJson = join(pkg, "jsr.json");
+	const jsrName = name.startsWith(NS) ? name : `${NS}/${name}`;
 	const config = {
-		name,
 		version,
+		name: jsrName,
 		exports: "./src/index.ts",
 		publish: {
 			include: ["src/**/*.ts"],
