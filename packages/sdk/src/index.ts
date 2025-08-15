946092bfde09532380be4ea31f7470e05384f15a
Julian Mills <julian.exodius@gmail.com>
2025-08-15 12:56:31 +0200

    restructure Surreal class

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,7 +2,6 @@ export * from "./cbor";
 export * from "./engine";
 export * from "./errors";
 export * from "./surreal";
-export { Surreal as default } from "./surreal";
 export * from "./types";
 export * from "./utils";
 export * from "./value";
