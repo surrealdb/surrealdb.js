bc47102d7dfd4f4d3ea9db829e0254f374c4cda9
Julian Mills <julian.exodius@gmail.com>
2025-10-30 16:16:53 +0100

    export session

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./cbor";
 export * from "./engine";
 export * from "./errors";
+export * from "./session";
 export * from "./surreal";
 export * from "./types";
 export * from "./utils";
