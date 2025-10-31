2b9d0b58daeb069d62e7bbcdb104e1dd0f4d1798
Julian Mills <julian.exodius@gmail.com>
2025-10-31 11:36:00 +0100

    version guard tests

--- packages/tests/integration/values.test.ts
+++ packages/tests/integration/values.test.ts
@@ -1,13 +1,17 @@
 import { describe, expect, test } from "bun:test";
+import { satisfies } from "semver";
 import { RecordId } from "surrealdb";
-import { setupServer } from "./__helpers__";
+import { fetchVersion, setupServer } from "./__helpers__";
 
 const { createSurreal } = await setupServer();
 
 describe("values", async () => {
-    test("sets basics", async () => {
-        const surreal = await createSurreal();
+    const surreal = await createSurreal();
+    const version = await fetchVersion(surreal);
 
+    const is3x = satisfies(version, ">=3.0.0-alpha.1");
+
+    test.if(is3x)("sets basics", async () => {
         await surreal.create(new RecordId("foo", 1)).content({
             set: new Set(["a", "b", "c"]),
         });
@@ -19,9 +23,7 @@ describe("values", async () => {
         expect(result.set).toEqual(new Set(["a", "b", "c"]));
     });
 
-    test("sets uniqueness", async () => {
-        const surreal = await createSurreal();
-
+    test.if(is3x)("sets uniqueness", async () => {
         await surreal.create(new RecordId("foo", 2)).content({
             set: new Set([new RecordId("hello", "world"), new RecordId("hello", "world")]),
         });
