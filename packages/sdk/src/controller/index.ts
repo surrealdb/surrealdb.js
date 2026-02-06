844c2f8785c4e42727d4d4f237976f7c78a0e789
Micha de Vries <micha@devrie.sh>
2026-02-06 10:19:05 +0100

    undefined

--- packages/sdk/src/controller/index.ts
+++ packages/sdk/src/controller/index.ts
@@ -314,14 +314,9 @@ export class ConnectionController implements SurrealProtocol, EventPublisher<Con
 
         const res = await this.#engine.use(what, session);
 
-        const namespace = res.namespace ?? what.namespace;
-        const database = res.database ?? what.database;
         const sessionState = this.getSession(session);
-
-        if (namespace === null) sessionState.namespace = undefined;
-        if (database === null) sessionState.database = undefined;
-        if (namespace) sessionState.namespace = namespace;
-        if (database) sessionState.database = database;
+        sessionState.namespace = res.namespace ?? what.namespace ?? undefined;
+        sessionState.database = res.database ?? what.database ?? undefined;
 
         const selected: NamespaceDatabase = {
             namespace: sessionState.namespace,
